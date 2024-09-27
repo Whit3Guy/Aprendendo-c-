3959b38d59f337fe79e940490fe696aaf48ce5b8
Gabriel <gabrielraimundo@id.uff.br>
2024-09-27 01:40:09 -0300

    the first program in csharp

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
 ﻿class Program{
     static void Main(){
+        string name;
+        int birthdate;
+        int age;
+
+
+        Console.Write("Insert your name: ");
+        name = Console.ReadLine();
+
+        Console.Write("Insert birthdate: ");
+        birthdate = int.Parse(Console.ReadLine());
+        
+        age = 2024 - birthdate;
+
+        Console.WriteLine($"Hi {name}!");
+
+        Console.WriteLine($"You have {age} years old");
+
+        if(age >= 18)
+        {
+        Console.WriteLine($"You are of legal age");
+        }
+        else
+        {
+        Console.WriteLine($"You aren't of legal age");
+
+        }
+
     }
 }
